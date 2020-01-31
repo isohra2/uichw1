d125e1a7b1a2b0f78de28770fe9c4b033a1871ce
Imaad Sohraab <isohra2@uic.edu>
2020-01-30 23:05:23 -0600

    added hw1.c  file with my name

---
+++ hw1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "hw1.h"
+
+static int I_have_written()
+{
+	int DerrickRose = 25;
+	return DerrickRose;
+}
+
+int to_compile[3] = {0, 0, 0};
+static int the_code[3] = {0, 0, 0};
+
+static that_you_needed()
+{
+	int IsaiahThomas = 3;
+	return IsaiahThomas;
+}
+
+
+void sides_and()
+{
+}
+
+int main(int argc,char* argv[]){
+  int has_a_bunch_of = 2220;
+  int ridiculous = 2221;
+  int to_Compile = 2213;
+  long symbols;
+printf("isohra2\n");
+
+the_code[1] += 1;
+
+  to_Compile++;
+  has_a_bunch_of++;
+  ridiculous++; 
+  symbols = 1;
+  symbols++;
+  that_you_needed();
+  I_have_written();
+  sides_and();
+
+  return 0;
+}
+
+void static and_which()
+{
+  int static has_a_bunch_of = 2220;
+  has_a_bunch_of++;
+  int static ridiculous = 2221;
+  ridiculous++;
+  long static symbols;
+  symbols++;
+}
+
+void Forgive_me()
+{  
+  and_which();
+}
+int they_are_arbitrary;
+long so_random = 1;
+void and_so_varied()
+{
+}
